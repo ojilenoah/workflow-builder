16e98e3ae7feee14fdcea74dd9369425211f0c87
Noah Ojile <noahojile04@gmail.com>
2025-04-27 23:22:38 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Workflow_builder',
+  description: 'Created by Noah Ojile',
+  generator: 'Noah Ojile',
 }
 
 export default function RootLayout({
